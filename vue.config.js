e617850ddda2f06bd9f6fd4d0f5aee7e958e2cb4
Nizamutdin <125039062+Usachann@users.noreply.github.com>
2024-07-07 21:51:03 +0300

    fixTercer

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,25 @@
+
+const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = {
     configureWebpack: {
       optimization: {
         minimize: true,
-      }
-    }
-  }
+        minimizer: [
+          new TerserPlugin({
+            terserOptions: {
+              ecma: undefined,
+              parse: {},
+              compress: {},
+              mangle: true,
+              module: false,
+              format: {
+                comments: false,
+              },
+            },
+          }),
+        ],
+      },
+    },
+  };
+  
